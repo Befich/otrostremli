ae376196bd05183906c31d176ddd2418663b6478
Befich <samuelcortez454@gmail.com>
2024-11-05 16:01:39 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -7,41 +7,24 @@ df = pd.read_csv("agriculture_dataset.csv")
 
 # Muestra un título y una descripción en la aplicación Streamlit.
 st.write("""
-# Datos sobre el Titanic
-## Gráficos usando la base de datos del Titanic
+# Comparacion entre dos tipos de cosechas
+## Gráficos usando la base de datos de agricultura
 """)
 
-# Usando la notación "with" para crear una barra lateral en la aplicación Streamlit.
-with st.sidebar:
-    # Título para la sección de opciones en la barra lateral.
-    st.write("# Opciones")
-    
-    # Crea un control deslizante (slider) que permite al usuario seleccionar un número de bins
-    # en el rango de 0 a 10, con un valor predeterminado de 2.
-    div = st.slider('Número de bins:', 0, 50, 2)
-    
-    # Muestra el valor actual del slider en la barra lateral.
-    st.write("Bins=", div)
 
-# Desplegamos un histograma con los datos del eje X
-fig, ax = plt.subplots(1, 2, figsize=(10, 3))
-ax[0].hist(df["Age"], bins=div, color="#CC7916")
-ax[0].set_xlabel("Edad")
-ax[0].set_ylabel("Frecuencia")
-ax[0].set_title("Histograma de edades")
 
-# Tomando datos para hombres y contando la cantidad
-df_male = df[df["Sex"] == "male"]
-cant_male = len(df_male)
+# Tomando datos para arroz y contando la cantidad
+df_rice = df[df["Crop_Type"] == "Rice"]
+cant_rice = len(df_rice)
 
-# Tomando datos para mujeres y contando la cantidad
-df_female = df[df["Sex"] == "female"]
-cant_female = len(df_female)
+# Tomando datos para Maiz y contando la cantidad
+df_maize = df[df["Crop_Type"] == "Maize"]
+cant_maize = len(df_maize)
 
-ax[1].bar(["Masculino", "Femenino"], [cant_male, cant_female], color = "#2EAEB8")
-ax[1].set_xlabel("Sexo")
+ax[1].bar(["Arroz", "Maiz"], [cant_rice, cant_maize], color = "#2EAEB8")
+ax[1].set_xlabel("Tipo de cosecha")
 ax[1].set_ylabel("Cantidad")
-ax[1].set_title('Distribución de hombres y mujeres')
+ax[1].set_title('Distribución entre arroz y maiz plantados')
 
 # Desplegamos el gráfico
 st.pyplot(fig)
