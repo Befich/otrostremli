e2b062a3f269c0ae9f71a0799a565126fd959001
Befich <samuelcortez454@gmail.com>
2024-11-05 22:52:15 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -30,6 +30,28 @@ if st.button("Arroz(foto)"):
 if st.button("Maiz(foto)"):
   st.image("maiztoiuhs.jpg", caption="Brotes de maiz")
 
+# Usando la notación "with" para crear una barra lateral en la aplicación Streamlit.
+with st.sidebar:
+    # Título para la sección de opciones en la barra lateral.
+    st.write("# Opciones")
+    
+    # Crea un control deslizante (slider) que permite al usuario seleccionar un número de bins
+    # en el rango de 0 a 10, con un valor predeterminado de 2.
+    div = st.slider('Número de bins:', 0, 50, 10)
+    
+    # Muestra el valor actual del slider en la barra lateral.
+    st.write("Bins=", div)
+
+# Desplegamos un histograma con los datos del eje X
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax.hist(df["Fertilizer_Used(tons)"], bins=div)
+ax.set_xlabel("Toneladas")
+ax.set_ylabel("Frecuencia")
+ax.set_title("Histograma Fertilizante usado")
+st.pyplot(fig)
+
+
+
 st.write("""
 ## Muestra de datos cargados
 """)
